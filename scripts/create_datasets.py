3b8300abd0082964120e237e70e9758c59e43630
john-chrosniak <chrosniakj@gmail.com>
2023-04-20 23:22:35 -0400

    finished scipt and added perturbed datasets

--- scripts/create_datasets.py
+++ scripts/create_datasets.py
@@ -17,8 +17,19 @@ def write_dataset():
             dropout = random.sample(range(len(points)), int(len(points) - len(points) * 0.05*i))
             points = points[dropout]
             with open(os.path.join("../data/racecar/perturbed_{}".format(i), file), 'wb') as pcd_file:
-                    points.ravel().astype('float32').tofile(pcd_file)
-    
+                points.ravel().astype('float32').tofile(pcd_file)
+    # Create perturbed kitti datasets
+    for i in range(1,4):
+        os.mkdir("../data/kitti/perturbed_{}".format(i))
+        for file in os.listdir("../data/kitti/velodyne"):
+            points = np.fromfile(os.path.join("../data/kitti/velodyne", file), dtype=np.float32).reshape(-1, 4)
+            # Gaussian noise
+            points += np.random.normal(0, i*0.5, points.shape)
+            # Dropout
+            dropout = random.sample(range(len(points)), int(len(points) - len(points) * 0.05*i))
+            points = points[dropout]
+            with open(os.path.join("../data/kitti/perturbed_{}".format(i), file), 'wb') as pcd_file:
+                points.ravel().astype('float32').tofile(pcd_file)
 
 
 
