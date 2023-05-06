393106028e5d1806465764450acf8ff40fa2c573
john-chrosniak <chrosniakj@gmail.com>
2023-05-06 17:26:18 -0400

    less noise more dropout

--- scripts/create_datasets.py
+++ scripts/create_datasets.py
@@ -12,9 +12,9 @@ def write_dataset():
         for file in os.listdir("../data/racecar/velodyne"):
             points = np.fromfile(os.path.join("../data/racecar/velodyne", file), dtype=np.float32).reshape(-1, 4)
             # Gaussian noise
-            points += np.random.normal(0, i*0.05, points.shape)
+            points += np.random.normal(0, i*0.01, points.shape)
             # Dropout
-            dropout = random.sample(range(len(points)), int(len(points) - len(points) * 0.05*i))
+            dropout = random.sample(range(len(points)), int(len(points) - len(points) * 0.1*i))
             points = points[dropout]
             with open(os.path.join("../data/racecar/perturbed_{}".format(i), file), 'wb') as pcd_file:
                 points.ravel().astype('float32').tofile(pcd_file)
